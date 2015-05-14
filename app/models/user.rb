9b9a47b00afe88f45dadcdac7942950f48f923fc
Luca Hessel <l.hessel@gmx.de>
2015-05-14 19:58:49 +0200

    added relations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   validates :username,  presence: true, length: { maximum: 50 }
   has_secure_password
   validates :password, length: { minimum: 6 }
+	has_many :quizzes
 	
 	def validuser
 		return true
