34872e50a033de7b617369d45b60f887b12464d3
Luca Hessel <l.hessel@gmx.de>
2015-05-25 22:07:56 +0200

    test if valid question is returned by get_random_question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ class Question < ActiveRecord::Base
   validates_presence_of :question, :answer0, :answer1, :answer2, :answer3, :correctAnswer, :difficulty
 	
 	def self.get_random_question(diff)
-		@questions = Question.select("id, question, answer0, answer1, answer2, answer3").where(difficulty: diff).all
+		@questions = Question.select("id, question, answer0, answer1, answer2, answer3, correctAnswer, difficulty").where(difficulty: diff).all
 		
 		@question = @questions[rand(@questions.length) - 1]
 	end
--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+	test "gets random question" do
+		@question = Question.get_random_question(1)
+		assert @question.valid?
+	end
+	
 end
